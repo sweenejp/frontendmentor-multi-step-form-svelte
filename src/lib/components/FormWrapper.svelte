b7c24bc9bbddff7c9e5cc7a0d93aca7733f43e6c
james.sweeney <james.sweeney@liquidweb.com>
2024-01-01 06:26:42 -0600

    add stepper component

--- src/lib/components/FormWrapper.svelte
+++ src/lib/components/FormWrapper.svelte
@@ -1,10 +1,13 @@
 <script>
+	import Stepper from './Stepper.svelte';
+
 	/** @type {string} */
 	export let title;
 	/** @type {string} */
 	export let subtitle;
 </script>
 
+<Stepper />
 <h1>{title}</h1>
 <h2>{subtitle}</h2>
 <form>
---
+++ src/lib/components/Stepper.svelte
@@ -0,0 +1,14 @@
+<div>
+	<div>
+		<span>1</span><span>Step 1</span><span>Your Info</span>
+	</div>
+	<div>
+		<span>2</span><span>Step 2</span><span>Select Plan</span>
+	</div>
+	<div>
+		<span>3</span><span>Step 3</span><span>Add-ons</span>
+	</div>
+	<div>
+		<span>4</span><span>Step 4</span><span>Summary</span>
+	</div>
+</div>
