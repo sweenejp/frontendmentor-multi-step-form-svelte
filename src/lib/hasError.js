4c0b35d24ff5fbc8a2bf6d5ebcb2b365c0a7fbfe
james.sweeney <james.sweeney@liquidweb.com>
2024-01-02 07:57:30 -0600

    adjust hasError return strings

--- src/lib/hasError.js
+++ src/lib/hasError.js
@@ -20,15 +20,15 @@ export default function hasError(field) {
 	if (validity.valid) return;
 
 	// If field is required and empty
-	if (validity.valueMissing) return 'Please fill out this field.';
+	if (validity.valueMissing) return 'This field is required';
 
 	// If not the right type
 	if (validity.typeMismatch) {
 		// Email
-		if (field.type === 'email') return 'Please enter an email address.';
+		if (field.type === 'email') return 'Please enter an email address';
 
 		// URL
-		if (field.type === 'url') return 'Please enter a URL.';
+		if (field.type === 'url') return 'Please enter a URL';
 	}
 
 	// If too short
@@ -38,7 +38,7 @@ export default function hasError(field) {
 			field.getAttribute('minLength') +
 			' characters or more. You are currently using ' +
 			field.value.length +
-			' characters.'
+			' characters'
 		);
 
 	// If too long
@@ -48,22 +48,22 @@ export default function hasError(field) {
 			field.getAttribute('maxLength') +
 			' characters. You are currently using ' +
 			field.value.length +
-			' characters.'
+			' characters'
 		);
 
 	// If number input isn't a number
-	if (validity.badInput) return 'Please enter a number.';
+	if (validity.badInput) return 'Please enter a number';
 
 	// If a number value doesn't match the step interval
 	if (validity.stepMismatch) return 'Please select a valid value.';
 
 	// If a number field is over the max
 	if (validity.rangeOverflow)
-		return 'Please select a value that is no more than ' + field.getAttribute('max') + '.';
+		return 'Please select a value that is no more than ' + field.getAttribute('max');
 
 	// If a number field is below the min
 	if (validity.rangeUnderflow)
-		return 'Please select a value that is no less than ' + field.getAttribute('min') + '.';
+		return 'Please select a value that is no less than ' + field.getAttribute('min');
 
 	// If pattern doesn't match
 	if (validity.patternMismatch) {
@@ -71,9 +71,9 @@ export default function hasError(field) {
 		if (field.hasAttribute('title')) return field.getAttribute('title');
 
 		// Otherwise, generic error
-		return 'Please match the requested format.';
+		return 'Please match the requested format';
 	}
 
 	// If all else fails, return a generic catchall error
-	return 'The value you entered for this field is invalid.';
+	return 'The value you entered for this field is invalid';
 }
