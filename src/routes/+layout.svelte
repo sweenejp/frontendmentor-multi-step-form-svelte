acee2d2e45c70a11b41c858417a1ffc1742a9cfa
james.sweeney <james.sweeney@liquidweb.com>
2024-01-04 14:20:39 -0600

    git rid of global styles warning

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,53 @@
 </div>
 
 <style>
+	@font-face {
+		font-family: 'Ubuntu';
+		font-weight: 700;
+		font-style: normal;
+		src: url('/fonts/Ubuntu-Bold.ttf');
+	}
+
+	@font-face {
+		font-family: 'Ubuntu';
+		font-weight: 500;
+		font-style: normal;
+		src: url('/fonts/Ubuntu-Medium.ttf');
+	}
+
+	@font-face {
+		font-family: 'Ubuntu';
+		font-weight: 400;
+		font-style: normal;
+		src: url('/fonts/Ubuntu-Regular.ttf');
+	}
+	:root {
+		--marine-blue: hsl(213, 96%, 18%);
+		--purplish-blue: hsl(243, 100%, 62%);
+		--light-blue: hsl(206, 94%, 87%);
+		--strawberry-red: hsl(354, 84%, 57%);
+		--cool-gray: hsl(231, 11%, 63%);
+		--light-gray: hsl(229, 24%, 87%);
+		--magnolia: hsl(217, 100%, 97%);
+		--alabaster: hsl(231, 100%, 99%);
+	}
+
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		font-family: 'Ubuntu', sans-serif;
+		line-height: 1.3;
+	}
+
+	:global(body) {
+		background-color: var(--magnolia);
+
+		@media (min-width: 800px) {
+			background-color: white;
+		}
+	}
+
 	.content-container {
 		top: -70px;
 		position: relative;
