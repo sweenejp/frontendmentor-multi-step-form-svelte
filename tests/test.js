e2e69add55a87c20ccba2a3515bc6b6f3be94a01
james.sweeney <james.sweeney@liquidweb.com>
2024-01-04 07:12:53 -0600

    fix tests

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Personal Info' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Personal info' })).toBeVisible();
 });
 
 test('personal info form validation', async ({ page }) => {
@@ -10,20 +10,20 @@ test('personal info form validation', async ({ page }) => {
 	await page.getByLabel('Name').fill('Jimmy');
 	await page.getByText('Next').click();
 	await expect(page.getByRole('heading', { name: 'Personal Info' })).toBeVisible();
-	await expect(page.getByText('Please fill out this field').first()).toBeVisible();
-	await expect(page.getByText('Please fill out this field').nth(1)).toBeVisible();
+	await expect(page.getByText('This field is required').first()).toBeVisible();
+	await expect(page.getByText('This field is required').nth(1)).toBeVisible();
 });
 
 test('personal info form validation - must have valid email', async ({ page }) => {
 	await page.goto('/');
 	await page.getByLabel('Email').fill('meow');
 	await page.getByText('Next').click();
-	await expect(page.getByRole('heading', { name: 'Personal Info' })).toBeVisible();
-	await expect(page.getByText('Please enter an email address.')).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Personal info' })).toBeVisible();
+	await expect(page.getByText('Please enter an email address')).toBeVisible();
 
 	await page.getByLabel('Email').fill('meow@meow.com');
 	// goes away after entering valid email
-	await expect(page.getByText('Please enter an email address.')).not.toBeVisible();
+	await expect(page.getByText('Please enter an email address')).not.toBeVisible();
 });
 
 test('full end to end', async ({ page }) => {
@@ -33,7 +33,7 @@ test('full end to end', async ({ page }) => {
 	await page.getByLabel('Phone Number').fill('123-123-1234');
 	await page.getByRole('button', { name: 'Next Step' }).click();
 	await page.getByText('Advanced').click();
-	await page.getByText('Yearly Cycle').click();
+	await page.getByText('Monthly', { exact: true }).click();
 	await page.getByRole('button', { name: 'Next Step' }).click();
 	await page.getByText('Larger storage').click();
 	await page.getByText('Online service').click();
@@ -41,7 +41,7 @@ test('full end to end', async ({ page }) => {
 	await page.getByRole('button', { name: 'Change' }).click();
 	await page.getByRole('button', { name: 'Next Step' }).click();
 	await page.getByRole('button', { name: 'Go Back' }).click();
-	await page.getByLabel('Pro').check();
+	await page.getByText('Pro').click();
 	await page.getByRole('button', { name: 'Next Step' }).click();
 	await page.getByRole('button', { name: 'Next Step' }).click();
 	await page.getByRole('button', { name: 'Confirm' }).click();
